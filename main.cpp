683874882d308672bf7288b3664dd3feae0faacb
luozhen <lzh768.earth@gmail.com>
2017-07-18 19:11:06 +0800

    next round cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "TheatreSquare.h"
 #include "Watermelon.h"
 #include "Way_Too_Long_Words.h"
+#include "Next_round.h"
 using namespace std;
 
 int main() {
 
     cout << "Hello, World!" << std::endl;
 
-    wtlw_solution();
+    cout << nr_solution() << endl;
     return 0;
 }
