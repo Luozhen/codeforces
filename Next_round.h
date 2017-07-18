cc3adfa13cb6585f2876d6116c7aaed276ad07c1
luozhen <lzh768.earth@gmail.com>
2017-07-18 16:03:41 +0800

    way too long words

---
+++ Next_round.h
@@ -0,0 +1,8 @@
+//
+// Created by luozhen on 2017/7/18.
+//
+
+#ifndef CODEFORCES_NEXT_ROUND_H
+#define CODEFORCES_NEXT_ROUND_H
+
+#endif //CODEFORCES_NEXT_ROUND_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "TheatreSquare.h"
 #include "Watermelon.h"
+#include "Way_Too_Long_Words.h"
 using namespace std;
 
 int main() {
 
     cout << "Hello, World!" << std::endl;
 
-    cout << water_solution() << endl;
+    wtlw_solution();
     return 0;
 }
