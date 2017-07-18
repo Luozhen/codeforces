502372cd0a9a61c4f7016e0e855bffcc617cef37
luozhen <lzh768.earth@gmail.com>
2017-07-18 15:43:11 +0800

    watermelon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "TheatreSquare.h"
+#include "Watermelon.h"
 using namespace std;
 
 int main() {
 
     cout << "Hello, World!" << std::endl;
 
-    cout << ts_solution() << endl;
+    cout << water_solution() << endl;
     return 0;
 }
