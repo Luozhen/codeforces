b381349202955136927f27561fcd80caad7e2915
luozhen <lzh768.earth@gmail.com>
2017-07-19 10:27:59 +0800

    domino piling

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@
 #include "Way_Too_Long_Words.h"
 #include "Next_round.h"
 #include "String_Task.h"
+#include "Domino_piling.h"
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << std::endl;
 
-    cout << st_solution() << endl;
+    cout << dp_solution() << endl;
     return 0;
 }
