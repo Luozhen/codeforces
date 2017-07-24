d0f493465ccf059dffd526b8ad7da562ec621f7a
luozhen <lzh768.earth@gmail.com>
2017-07-24 14:09:14 +0800

    xor rule

--- Helpful_Maths.h
+++ Helpful_Maths.h
@@ -79,6 +79,7 @@ int hm_solution(){
             sum[k] = '0' + array[h++];
         }
     }
+    // for(i=0;s[i];i+=2)for(j=i;s[j];j+=2)if(s[i]>s[j])s[i]^=s[j]^=s[i]^=s[j];
     cout << sum << endl;
     return 0;
 }
